a0d53d4bc3b5dfa20821e2d97d93adb88e30cf7d
anant0712 <anant0761@gmail.com>
2026-03-30 08:45:31 +0530

    first commit

---
+++ accounts/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+# Create your models here.
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, email, username, password=None):
+        if not email:
+            raise ValueError("Email cannot be None")
+        if not username:
+            raise ValueError("User must have Username ")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, username, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True, blank=True, null=True)
+
+
+    # required
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    # The last four fields are important while creating a Custom User model
+
+    USERNAME_FIELD = 'email'
+    # This USERNAME_FIELD = 'email' will allow us to login using Email Address
+
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    # Now we have to tell that we are using MyAccountManager here so we mentioned objects= MyAccountManager
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
